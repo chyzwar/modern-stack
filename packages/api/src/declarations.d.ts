e1c176de7ad43724a6742951b199f6de4936d325
chyzwar <schizek.marcin@gmail.com>
2020-03-08 16:22:30 +0000

    chore(): better start command

---
+++ packages/api/src/declarations.d.ts
@@ -0,0 +1,12 @@
+
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: 'development' | 'production';
+      PORT: string;
+    }
+  }
+}
+
+export {}
--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -1,11 +1,15 @@
-import * as fastify from 'fastify'
-import { Server, IncomingMessage, ServerResponse } from 'http'
+import fastify from 'fastify'
 
+import { 
+  Server, 
+  IncomingMessage, 
+  ServerResponse 
+} from 'http'
 
 const server: fastify.FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({
-  
-})
+  logger: {
+    prettyPrint: process.env.NODE_ENV === 'development',
+  },
+});
 
-server.listen(() => {
-  console.log('server started')
-})
+server.listen(process.env.PORT);
