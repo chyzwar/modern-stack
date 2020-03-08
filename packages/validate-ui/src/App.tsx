26824d39fa8e620da7815de86b91edad84d5ddb0
chyzwar <schizek.marcin@gmail.com>
2020-03-08 19:45:25 +0000

    chore(): added signup in materia ui

--- packages/validate-ui/src/App.tsx
+++ packages/validate-ui/src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from './views/Login'
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <BrowserRouter>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+          </Switch>
+      </BrowserRouter>
+    );
+  }
 
 export default App;
