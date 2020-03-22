9658b67b08b3942849a8873ad68091b36f164e17
chyzwar <schizek.marcin@gmail.com>
2020-03-22 19:38:58 +0000

    fix(): admin-ui not building

--- packages/ui/src/views/Login.tsx
+++ packages/ui/src/views/Login.tsx
@@ -13,7 +13,7 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
-function Copyright() {
+function Copyright(): React.ReactElement {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
